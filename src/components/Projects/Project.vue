5a22bd4cb557f931a5bb48a48c1dd0e95756c7b2
João Victor <joaovicdsantos@gmail.com>
2020-12-30 11:25:09 -0300

    perdi meus commits por nao ter dado push :(

---
+++ src/components/Projects/Project.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="project">
+    <div class="project__image">
+      <img src="../../assets/budajuda.budaportfolio.epizy.com.png" alt="" style="width: 400px">
+    </div>
+    <div class="project__content">
+      Já é amargo o sabor de te amar
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.project {
+  background-color: #fff;
+  border-radius: 10px;
+}
+.project__image {
+
+}
+.project__content {
+
+}
+</style>
